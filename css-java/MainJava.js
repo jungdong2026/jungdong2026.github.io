eca6ab82c6d441a4ef8e2adba15d18e1fd46b571
jungdong2026 <dong3640@hanmail.net>
2026-02-07 01:34:04 +0900

    Auto commit from TC

--- css-java/MainJava.js
+++ css-java/MainJava.js
@@ -108,18 +108,18 @@ $(document).ready(function () {
             });
         });
 
-        $(document).ready(function () {
+            $(document).ready(function () {
             $('.nav-11 a').click(function () {
-                $('#wrapper').load('https://jungdong2026.github.io/GuestBook/GuestBook.html', function () {
+                $('#wrapper').load('https://jungdong2026.github.io/humorBoard/Humor.html', function () {
                     $(this).fadeIn();
                 });
                 return false;
             });
         });
 
-        $(document).ready(function () {
+            $(document).ready(function () {
             $('.nav-12 a').click(function () {
-                $('#wrapper').load('https://jungdong2026.github.io/MusicBox/MusicBox.html', function () {
+                $('#wrapper').load('https://jungdong2026.github.io/GuestBook/GuestBook.html', function () {
                     $(this).fadeIn();
                 });
                 return false;
